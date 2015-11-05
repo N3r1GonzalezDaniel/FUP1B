5950b325578bc19c05ae7a17d242769bf267625b
N3r1GonzalezDaniel <nerigonzalezdaniel@hotmail.com>
2015-11-04 18:43:06 -0600

    Create separa_numeros.c

---
+++ separa_numeros.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<conio.h>
+
+int x,y,z,u,d,c,m;
+char S;
+int main()
+
+{//inico
+
+    do{
+	printf("Programa que te dice las unidades, decenas, centenas y millares de un numero\n\n ");
+	printf("Ingrese su numero:");
+	scanf("%d",&x);
+	
+	u = x % 10;
+	printf("Unidades:%d\n",u);
+	
+	y = x - u;
+	y = y % 100;
+	d = y / 10;
+	printf("Decenas:%d\n",d);
+	
+	y = x - d*10 - u;
+	z = y % 1000;
+	c = (z / 10) / 10;
+	printf("Centenas:%d\n",c);
+	
+	y = x - d*10 - c*10 - u;
+	m = y / 1000;
+    printf("Millares:%d\n",m);
+    
+    do{
+	printf("Deseas continuar?[s/n]:\n");
+   	fflush(stdin);
+   	scanf("%c",&S);
+   	printf("\n\n");
+   	} while (S=='\n');
+	}  while(S=='s');    
+    
+	printf("Grasias por usar el programa");
+}//fin
