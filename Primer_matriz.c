26b5c64c03cafc84a76389c5b38e2cd4cf0922b5
N3r1GonzalezDaniel <nerigonzalezdaniel@hotmail.com>
2015-12-01 15:16:50 -0600

    Primer_Matriz.c

---
+++ Primer_matriz.c
@@ -0,0 +1,26 @@
+//Matrices
+// int indentidad[filas][columnas]
+#include<stdio.h>
+
+int identidad[3][3],i,j;
+int main()
+   {
+   	identidad[0][0]=1;
+   	identidad[0][1]=0;
+   	identidad[0][2]=0;
+   	identidad[1][0]=0;
+   	identidad[1][1]=1;
+   	identidad[1][2]=0;
+   	identidad[2][0]=0;
+   	identidad[2][1]=0;
+   	identidad[2][2]=1;
+   	
+   	 for(i=0;i<3;i++)
+   	 {
+   	      for(j=0;j<3;j++)
+   	       {
+   	         printf("%d ",identidad[i][j]);	
+		   }
+		   printf("\n");
+	 }
+   }
